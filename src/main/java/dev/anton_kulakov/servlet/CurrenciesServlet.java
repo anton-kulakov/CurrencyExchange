65c11209cabae3ef5c65f29d84a2c05acd7cefaf
anton kulakov <kulakov-akv@yandex.ru>
2024-06-04 13:05:15 +0300

    feat: add CurrenciesServlet

---
+++ src/main/java/dev/anton_kulakov/servlet/CurrenciesServlet.java
@@ -0,0 +1,23 @@
+package dev.anton_kulakov.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+
+@WebServlet(name = "currenciesServlet", value = "/currencies")
+public class CurrenciesServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
