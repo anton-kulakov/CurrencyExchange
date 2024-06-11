a5fbecf7fa9f902a8d026963e2bdc364b5499f4e
anton kulakov <kulakov-akv@yandex.ru>
2024-06-11 15:57:29 +0300

    feat: implement init() method in ExchangeRatesServlet

--- src/main/java/dev/anton_kulakov/servlet/ExchangeRatesServlet.java
+++ src/main/java/dev/anton_kulakov/servlet/ExchangeRatesServlet.java
@@ -8,6 +8,15 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class ExchangeRatesServlet extends HttpServlet {
+    private static final String URL = "jdbc:sqlite:C:/Users/anton/IdeaProjects/CurrencyExchange/src/main/resources/database.db";
+
+    public void init() throws ServletException {
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            throw new ServletException("Failed to load SQLite JDBC driver", e);
+        }
+    }
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
