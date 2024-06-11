70fd650a7b366051cd74cc077a517faf15fd365d
anton kulakov <kulakov-akv@yandex.ru>
2024-06-11 21:04:46 +0300

    feat: add getters and setters in ExchangeRate class

--- src/main/java/model/ExchangeRate.java
+++ src/main/java/model/ExchangeRate.java
@@ -2,43 +2,36 @@ package model;
 
 public class ExchangeRate {
     int id;
-    int baseCurrencyID;
-    int targetCurrencyID;
+    Currency baseCurrency;
+    Currency targetCurrency;
     double rate;
-
-    public ExchangeRate(int id, int baseCurrencyID, int targetCurrencyID, double rate) {
-        this.id = id;
-        this.baseCurrencyID = baseCurrencyID;
-        this.targetCurrencyID = targetCurrencyID;
-        this.rate = rate;
-    }
 
     public int getId() {
         return id;
-    }
-
-    public int getBaseCurrencyID() {
-        return baseCurrencyID;
-    }
-
-    public int getTargetCurrencyID() {
-        return targetCurrencyID;
-    }
-
-    public double getRate() {
-        return rate;
     }
 
     public void setId(int id) {
         this.id = id;
     }
 
-    public void setBaseCurrencyID(int baseCurrencyID) {
-        this.baseCurrencyID = baseCurrencyID;
+    public Currency getBaseCurrency() {
+        return baseCurrency;
     }
 
-    public void setTargetCurrencyID(int targetCurrencyID) {
-        this.targetCurrencyID = targetCurrencyID;
+    public void setBaseCurrency(Currency baseCurrency) {
+        this.baseCurrency = baseCurrency;
+    }
+
+    public Currency getTargetCurrency() {
+        return targetCurrency;
+    }
+
+    public void setTargetCurrency(Currency targetCurrency) {
+        this.targetCurrency = targetCurrency;
+    }
+
+    public double getRate() {
+        return rate;
     }
 
     public void setRate(double rate) {
