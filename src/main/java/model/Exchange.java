cec614162f371acbdd97bf65ad584403ddbcda0f
anton kulakov <kulakov-akv@yandex.ru>
2024-06-15 19:26:48 +0300

    feat: add Exchange class

---
+++ src/main/java/model/Exchange.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Exchange {
+    private Currency baseCurrency;
+    private Currency targetCurrency;
+    private double rate;
+    private double amount;
+    private double convertedAmount;
+}
