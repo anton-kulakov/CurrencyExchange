e776b35f3784565064372b9f9fdb46fb3572a5c7
anton kulakov <kulakov-akv@yandex.ru>
2024-08-05 08:20:05 +0300

    feat: add CurrencyDAO/ExchangeRateDAO in ExchangeRatesServlet

--- src/main/java/dev/anton_kulakov/servlet/CurrencyServlet.java
+++ src/main/java/dev/anton_kulakov/servlet/CurrencyServlet.java
@@ -21,7 +21,7 @@ public class CurrencyServlet extends HttpServlet {
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String pathInfo = req.getPathInfo();
         String[] pathParts = pathInfo.split("/");
         String currencyCode = pathParts[1];
--- src/main/java/dev/anton_kulakov/servlet/ExchangeRatesServlet.java
+++ src/main/java/dev/anton_kulakov/servlet/ExchangeRatesServlet.java
@@ -1,22 +1,20 @@
 package dev.anton_kulakov.servlet;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import dao.CurrencyDAO;
+import dao.ExchangeRateDAO;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import model.Currency;
 import model.ExchangeRate;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.*;
-import java.util.ArrayList;
+import java.math.BigDecimal;
 import java.util.List;
 
 public class ExchangeRatesServlet extends HttpServlet {
-    private static final String URL = "jdbc:sqlite:C:/Users/anton/IdeaProjects/CurrencyExchange/src/main/resources/database.db";
-
     public void init() throws ServletException {
         try {
             Class.forName("org.sqlite.JDBC");
@@ -24,59 +22,14 @@ public class ExchangeRatesServlet extends HttpServlet {
             throw new ServletException("Failed to load SQLite JDBC driver", e);
         }
     }
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        List<ExchangeRate> exchangeRateList = new ArrayList<>();
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
+        ExchangeRateDAO exchangeRateDAO = ExchangeRateDAO.getInstance();
 
-        try (Connection connection = DriverManager.getConnection(URL)) {
-            String SQLQuery = "SELECT\n" +
-                    "    er.id,\n" +
-                    "    bc.id AS BaseCurrencyID,\n" +
-                    "    bc.code AS BaseCurrencyCode,\n" +
-                    "    bc.fullname AS BaseCurrencyFullName,\n" +
-                    "    bc.sign AS BaseCurrencySign,\n" +
-                    "    tc.id AS TargetCurrencyID,\n" +
-                    "    tc.code AS TargetCurrencyCode,\n" +
-                    "    tc.fullname AS TargetCurrencyFullName,\n" +
-                    "    tc.sign AS TargetCurrencySign,\n" +
-                    "    er.rate\n" +
-                    "FROM ExchangeRates er\n" +
-                    "INNER JOIN Currencies bc ON er.BaseCurrencyId = bc.id\n" +
-                    "INNER JOIN Currencies tc ON er.TargetCurrencyId = tc.id;";
-
-            try(Statement statement = connection.createStatement();
-                ResultSet resultSet = statement.executeQuery(SQLQuery)) {
-
-                while (resultSet.next()) {
-                    Currency baseCurrency = new Currency();
-                    Currency targetCurrency = new Currency();
-                    ExchangeRate exchangeRate = new ExchangeRate();
-
-                    baseCurrency.setId(resultSet.getInt("BaseCurrencyID"));
-                    baseCurrency.setCode(resultSet.getString("BaseCurrencyCode"));
-                    baseCurrency.setFullName(resultSet.getString("BaseCurrencyFullName"));
-                    baseCurrency.setSign(resultSet.getString("BaseCurrencySign"));
-
-
-                    targetCurrency.setId(resultSet.getInt("TargetCurrencyID"));
-                    targetCurrency.setCode(resultSet.getString("TargetCurrencyCode"));
-                    targetCurrency.setFullName(resultSet.getString("TargetCurrencyFullName"));
-                    targetCurrency.setSign(resultSet.getString("TargetCurrencySign"));
-
-                    exchangeRate.setId(resultSet.getInt("id"));
-                    exchangeRate.setBaseCurrency(baseCurrency);
-                    exchangeRate.setTargetCurrency(targetCurrency);
-                    exchangeRate.setRate(resultSet.getBigDecimal("rate"));
-
-                    exchangeRateList.add(exchangeRate);
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        String jsonExchangeRates = objectMapper.writeValueAsString(exchangeRateList);
+        List<ExchangeRate> exchangeRates = exchangeRateDAO.getAll();
+        String jsonExchangeRates = objectMapper.writeValueAsString(exchangeRates);
         resp.setContentType("application/json; charset=UTF-8");
         resp.setStatus(200);
         PrintWriter out = resp.getWriter();
@@ -84,50 +37,24 @@ public class ExchangeRatesServlet extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         req.setCharacterEncoding("UTF-8");
         resp.setContentType("application/json; charset=UTF-8");
 
         String baseCurrencyCode = req.getParameter("baseCurrencyCode");
         String targetCurrencyCode = req.getParameter("targetCurrencyCode");
-        double rate = Double.parseDouble(req.getParameter("rate"));
+        BigDecimal rate = new BigDecimal(req.getParameter("rate"));
 
-        String currencyIDQuery = "SELECT id FROM Currencies WHERE Code = ?";
-        String SQLQuery = "INSERT INTO ExchangeRates (BaseCurrencyID, TargetCurrencyID, Rate) " +
-                "VALUES (?, ?, ?)";
+        CurrencyDAO currencyDAO = CurrencyDAO.getInstance();
+        ExchangeRateDAO exchangeRateDAO = ExchangeRateDAO.getInstance();
 
-        try (Connection connection = DriverManager.getConnection(URL);
-        PreparedStatement baseCurrencyStatement = connection.prepareStatement(currencyIDQuery);
-        PreparedStatement targetCurrencyStatement = connection.prepareStatement(currencyIDQuery);
-        PreparedStatement addExchangeRateStatement = connection.prepareStatement(SQLQuery)) {
+        exchangeRateDAO.save(new ExchangeRate(
+                0,
+                currencyDAO.getByCode(baseCurrencyCode).get(),
+                currencyDAO.getByCode(targetCurrencyCode).get(),
+                rate
+        ));
 
-            baseCurrencyStatement.setString(1, baseCurrencyCode);
-            targetCurrencyStatement.setString(1, targetCurrencyCode);
-
-            int baseCurrencyID = 0;
-            int targetCurrencyID = 0;
-
-            try (ResultSet resultSet = baseCurrencyStatement.executeQuery()) {
-                while (resultSet.next()) {
-                    baseCurrencyID = resultSet.getInt("ID");
-                }
-            }
-
-            try (ResultSet resultSet = targetCurrencyStatement.executeQuery()) {
-                while (resultSet.next()) {
-                    targetCurrencyID = resultSet.getInt("ID");
-                }
-            }
-
-            addExchangeRateStatement.setInt(1, baseCurrencyID);
-            addExchangeRateStatement.setInt(2, targetCurrencyID);
-            addExchangeRateStatement.setDouble(3, rate);
-
-            addExchangeRateStatement.executeUpdate();
-
-            resp.sendRedirect(req.getContextPath() + "/exchangeRate/" + baseCurrencyCode + targetCurrencyCode);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        resp.sendRedirect(req.getContextPath() + "/exchangeRate/" + baseCurrencyCode + targetCurrencyCode);
     }
 }
