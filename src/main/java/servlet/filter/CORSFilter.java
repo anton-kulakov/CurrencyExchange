0f2c99dd830ab9049ee2f95c713ff54ab8625f16
anton kulakov <kulakov-akv@yandex.ru>
2024-08-18 21:31:14 +0300

    chore: move filters in filter-package

--- src/main/java/servlet/filter/CORSFilter.java
+++ src/main/java/servlet/filter/CORSFilter.java
@@ -1,4 +1,4 @@
-package servlet;
+package servlet.filter;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServlet;
--- src/main/java/servlet/filter/EncodingFilter.java
+++ src/main/java/servlet/filter/EncodingFilter.java
@@ -1,4 +1,4 @@
-package servlet;
+package servlet.filter;
 
 import jakarta.servlet.*;
 
