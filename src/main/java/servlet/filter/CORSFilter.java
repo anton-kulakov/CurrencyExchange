80e5b9a9a8e11260bbccb3a7fb7d8601607cd4d5
anton kulakov <kulakov-akv@yandex.ru>
2024-08-19 10:03:03 +0300

    fix: add PATCH method processing to CORSFilter

--- src/main/java/servlet/filter/CORSFilter.java
+++ src/main/java/servlet/filter/CORSFilter.java
@@ -16,7 +16,7 @@ public class CORSFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse resp = (HttpServletResponse) servletResponse;
         resp.setHeader("Access-Control-Allow-Origin", "*");
-        resp.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        resp.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
         resp.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
         filterChain.doFilter(servletRequest, servletResponse);
     }
