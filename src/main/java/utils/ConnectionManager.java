c1139d2c15ee5471b6dcfcaa2eeff27a5a3e7af7
anton kulakov <kulakov-akv@yandex.ru>
2024-08-25 16:34:36 +0300

    fix: add the inclusion of foreign keys

--- src/main/java/utils/ConnectionManager.java
+++ src/main/java/utils/ConnectionManager.java
@@ -1,15 +1,14 @@
 package utils;
 
 import com.zaxxer.hikari.HikariDataSource;
+import lombok.Setter;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public final class ConnectionManager {
+    @Setter
     private static HikariDataSource dataSource;
-    public static void setDataSource(HikariDataSource ds) {
-        dataSource = ds;
-    }
 
     public static Connection getConnection() throws SQLException {
         return dataSource.getConnection();
